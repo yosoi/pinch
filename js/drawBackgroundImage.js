f746a4e5266d61a91d49f26d1552b08681e67a3c
Yosoi <yosoi@protonmail.com>
2020-05-30 21:08:15 -0400

    card improvements

--- js/drawBackgroundImage.js
+++ js/drawBackgroundImage.js
@@ -1,8 +1,17 @@
 function main(canvasId) {
   let canvas = document.getElementById(canvasId);
   let context = canvas.getContext("2d");
-  context.fillStyle = "magenta";
+
+  // draw background
+  context.fillStyle = "#134074";
   context.fillRect(0, 0, canvas.width, canvas.height);
+
+  // draw planet
+  context.beginPath();
+  context.arc(0, canvas.width*2 + canvas.height/3, canvas.width * 2, 0, 2 * Math.PI);
+  context.fillStyle = "#71a9f7";
+  context.fill();
+
   // var c = document.getElementById(canvasId);
   // var ctx = c.getContext("2d");
   //
@@ -14,6 +23,14 @@ function main(canvasId) {
   // // Fill with gradient
   // ctx.fillStyle = grd;
   // ctx.fillRect(0, 0, c.width, c.height);
+}
+
+function drawBackground(canvas, context) {
+
+}
+
+function drawPlanet(context) {
+
 }
 
 export default main;
