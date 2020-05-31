452e333821a79f983bb49d48a46810bce287f835
Yosoi <yosoi@protonmail.com>
2020-05-31 15:42:15 -0400

    sass integration

--- js/drawBackgroundImage.js
+++ js/drawBackgroundImage.js
@@ -11,26 +11,6 @@ function main(canvasId) {
   context.arc(0, canvas.width*2 + canvas.height/3, canvas.width * 2, 0, 2 * Math.PI);
   context.fillStyle = "#71a9f7";
   context.fill();
-
-  // var c = document.getElementById(canvasId);
-  // var ctx = c.getContext("2d");
-  //
-  // // Create gradient
-  // var grd = ctx.createLinearGradient(0, 0, c.width, 0);
-  // grd.addColorStop(0, "dark gray");
-  // grd.addColorStop(1, "gray");
-  //
-  // // Fill with gradient
-  // ctx.fillStyle = grd;
-  // ctx.fillRect(0, 0, c.width, c.height);
-}
-
-function drawBackground(canvas, context) {
-
-}
-
-function drawPlanet(context) {
-
 }
 
 export default main;
